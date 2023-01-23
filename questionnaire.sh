a68cddf1a27594725791a47e0b02f7969d56bab3
abdullahelshoura <36606092+abdullahelshoura@users.noreply.github.com>
2023-01-23 12:51:43 +0200

    Update questionnaire.sh

--- questionnaire.sh
+++ questionnaire.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
-# Program to tell a persons fortune
+echo -e "\n~~ Questionnaire ~~\n"
 
-echo -e "\n~~ Fortune Teller ~~\n"
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
 
-RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( RANDOM % 6 ))
+echo $QUESTION1
+read NAME
 
-function GET_FORTUNE() {
-  if [[ ! $1 ]]
-  then
-    echo Ask a yes or no question:
-  else
-    echo Try again. Make sure it ends with a question mark:
-  fi
+echo $QUESTION2
+read LOCATION
 
-  read QUESTION
-}
+echo $QUESTION3
+read WEBSITE
 
-GET_FORTUNE
-
-until [[ $QUESTION =~ \?$ ]]
-do
-  GET_FORTUNE again
-done
-
-echo -e "\n$RESPONSES[$N]"
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
